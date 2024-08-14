47ae5e84a85de20fa91bc8fcce897cf62ea7571c
Fabrizio <fabriziofontana02@gmail.com>
2024-08-14 16:01:56 +0200

    condizioni

---
+++ pt05.py
@@ -0,0 +1,14 @@
+#Condizioni
+x = 4
+if x == 5:
+    print('x è uguale a 5')
+elif x > 5:
+    print('x è maggiore di 5')
+else:
+    print('x è minore di 5')
+
+x = 12
+if x > 3 or x < 7:
+    print('x è maggiore di 3 o minore di 7')
+elif 11 < x < 20:
+    print('x è compreso tra 11 e 20')
