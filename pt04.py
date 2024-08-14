9abcfa35de16a0675508aa7d231abfcbb762c8cd
Fabrizio <fabriziofontana02@gmail.com>
2024-08-14 15:53:09 +0200

    operatori

---
+++ pt04.py
@@ -0,0 +1,28 @@
+import math
+
+#Operatori matematici
+x = 20
+y = 5
+print(x + y) #25
+print(x - y) #15
+print(x * y) #100
+print(x / y) #4.0
+print(x % y) #0
+print(x ** y) #3200000 (elevamento a potenza)
+print(x // y) #4 (divisione senza resto)
+
+#Operatori di assegnamento
+x = 20
+x += 5 #x = x + 5
+
+x = 16
+print(math.sqrt(x)) #4.0
+
+x = -5
+print(abs(x)) #5
+
+list = [1, 2, 3, 4, 5]
+print(len(list)) #5
+print(min(list)) #1
+print(max(list)) #5
+print(sum(list)) #15
