2d33cecc5b34793d9d1c27a4be93989963695e33
Fabrizio <fabriziofontana02@gmail.com>
2024-08-14 19:51:50 +0200

    arbitrary arguments e keyword arguments

--- pt08.py
+++ pt08.py
@@ -9,3 +9,20 @@ def somma(a, b):
 a = int(input("Inserisci un numero: "))
 b = int(input("Inserisci un altro numero: "))
 print("La somma è", somma(a, b))
+
+def stampa_ingredienti(*ingredienti):
+    print("Ingredienti:")
+    for ingrediente in ingredienti:
+        print("-", ingrediente.capitalize())
+stampa_ingredienti("farina", "uova", "zucchero")
+
+def fai_la_pasta(ricetta, sugo):
+    print(ricetta)
+    if sugo:
+        print("Anche il sugo")
+fai_la_pasta(sugo = False, ricetta = "Pasta al pomodoro")
+
+def saluta(nome = "Mondo"):
+    print("Ciao,", nome + "!")
+saluta()
+saluta("Alice")
