902faabd5a33b89c3c54e269a5d06e54bfbafb92
Fabrizio <fabriziofontana02@gmail.com>
2024-08-17 15:25:34 +0200

    math

---
+++ pt14.py
@@ -0,0 +1,24 @@
+#Math
+import math
+
+x = min(4, 0, 45)
+print(x) # 0
+
+x = max(4, 0, 45)
+print(x) # 45
+
+x = abs(-7.25)
+print(x) # 7.25
+
+x = pow(4, 3)
+print(x) # 64
+
+x = math.sqrt(64)
+print(x) # 8.0
+
+x = 64.80
+print(math.floor(x)) # 64
+print(math.ceil(x)) # 65
+
+print(math.pi) # 3.141592653589793
+
